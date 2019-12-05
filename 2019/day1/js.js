555c949f25e728cee988c80ffc90cb3f7e1a36ff
Aya <47668950+AyaPK@users.noreply.github.com>
2019-12-05 19:16:39 +0000

    Update js.js

--- 2019/day1/js.js
+++ 2019/day1/js.js
@@ -1,9 +1,9 @@
 
 var fs = require("fs");
 var text = fs.readFileSync("./input.txt", "utf-8");
-var textByLine = text.split("\n")
+var data = text.split("\n")
 
 output = []
-textByLine.forEach(a=>output.push(Math.floor(a/3)-2))
+data.forEach(a=>output.push(Math.floor(a/3)-2))
 console.log(output.reduce((a,b)=>a+b))
 
