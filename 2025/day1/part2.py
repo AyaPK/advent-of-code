4d4387d46dd5bbb6a0eb12d65c52a2997966b07b
Aya <Sherbnya@gmail.com>
2025-12-01 09:58:07 +0000

    Fix part 2

--- 2025/day1/part2.py
+++ 2025/day1/part2.py
@@ -1,26 +1,17 @@
 def parse(line):
-    line = line.strip()
-    return int(line[1:]) * (-1 if line[0] == "L" else 1)
+    sign = -1 if line[0] == "L" else 1
+    return sign * int(line[1:])
 
 moves = [parse(x) for x in open("input.txt")]
 
 pos = 50
 count = 0
 
-for move in moves:
-    if move > 0:
-        for x in range(pos, pos + move):
-            pos += 1
-            if pos > 99:
-                pos = 0
-            if pos == 0:
-                count += 1
-    elif move < 0:
-        for x in range(pos, pos + move, -1):
-            pos -= 1
-            if pos < 0:
-                pos = 99
-            if pos == 0:
-                count += 1
+for m in moves:
+    step = 1 if m > 0 else -1
+    for x in range(abs(m)):
+        pos = (pos + step) % 100
+        if pos == 0:
+            count += 1
 
 print(count)
